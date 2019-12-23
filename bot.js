a0fb4a913ba0e841931ef313dfc3dbe616c0040f
HKing2802 <croppa1@yahoo.com>
2019-12-23 14:18:59 -0500

    now working

--- bot.js
+++ bot.js
@@ -1,35 +1,52 @@
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
+
+var bk = false;
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == '=') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
 
-        args = args.splice(1);
+var bot = new Discord.Client();
+bot.login(auth.token);
+
+bot.on('ready', () => {
+    logger.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', (recievedMessage) => {
+    if (recievedMessage.author == bot.user) {
+        return
+        //prevent responding to its own message
+    }
+    if (recievedMessage.content.substring(0, 1) == ".") {
+        logger.info(recievedMessage.content.substring(1));
+        var cmdfull = recievedMessage.content.substring(1);
+        var cmdparsed = cmdfull.split(" ");
+        var cmd = cmdparsed[0];
+        var args = cmdparsed.splice(1);
+
+        var chan = recievedMessage.channel;
+
         switch (cmd) {
             case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
+                chan.send("Pong!");
+                break;
+            case 'endProcess':
+                chan.send("Killing Myself...");
+                bot.destroy()
+                break;
+            case 'repeat':
+                var rep = ""
+                for (var i = 0; i < args.length; i++) {
+                    rep = rep + args[i];
+                }
+                chan.send("Here it is again:\n" + rep);
                 break;
         }
     }
-});
+})
