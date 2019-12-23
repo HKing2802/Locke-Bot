3cb6910df3a625485422b5e992ce2e2d41edbcb5
HKing2802 <croppa1@yahoo.com>
2019-12-23 14:46:56 -0500

    added more commands, response shutdown and user checking for admin commands

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
 
-var bk = false;
+var active = true;
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -18,35 +18,71 @@ bot.on('ready', () => {
     logger.info(`Logged in as ${bot.user.tag}!`);
 });
 
+function process(recievedMessage) {
+    logger.info(recievedMessage.content.substring(1));
+    var cmdfull = recievedMessage.content.substring(1);
+    var cmdparsed = cmdfull.split(" ");
+    var cmd = cmdparsed[0];
+    var args = cmdparsed.splice(1);
+
+    var chan = recievedMessage.channel;
+    switch (cmd) {
+        case 'ping':
+            if (recievedMessage.author.username == "Icenoft") {
+                chan.send("Imagine .ping");
+            } else {
+                chan.send("Pong!");
+            }
+            break;
+        case 'endProcess':
+            if (recievedMessage.author.id == "324302699460034561") {
+                chan.send("Killing Myself...");
+                bot.destroy()
+            } else {
+                chan.send("You must be Admin to do that!");
+            }
+            break;
+        case 'repeat':
+            var rep = ""
+            for (var i = 0; i < args.length; i++) {
+                rep = rep + args[i] + " ";
+            }
+            chan.send("Here it is again:\n" + rep);
+            break;
+        case 'shutdown':
+            if (recievedMessage.author.id == "324302699460034561") {
+                chan.send("Stopping responses");
+                active = false;
+            } else {
+                chan.send("You must be Admin to do that!");
+            }
+            break;
+        case 'activate':
+            if (recievedMessage.author.id == "324302699460034561") {
+                chan.send("Restarting responses");
+                active = true;
+            } else {
+                chan.send("You must be Admin to do that!");
+            }
+            break;
+    }
+}
+
 bot.on('message', (recievedMessage) => {
     if (recievedMessage.author == bot.user) {
         return
         //prevent responding to its own message
     }
     if (recievedMessage.content.substring(0, 1) == ".") {
-        logger.info(recievedMessage.content.substring(1));
-        var cmdfull = recievedMessage.content.substring(1);
-        var cmdparsed = cmdfull.split(" ");
-        var cmd = cmdparsed[0];
-        var args = cmdparsed.splice(1);
-
-        var chan = recievedMessage.channel;
-
-        switch (cmd) {
-            case 'ping':
-                chan.send("Pong!");
-                break;
-            case 'endProcess':
-                chan.send("Killing Myself...");
-                bot.destroy()
-                break;
-            case 'repeat':
-                var rep = ""
-                for (var i = 0; i < args.length; i++) {
-                    rep = rep + args[i];
-                }
-                chan.send("Here it is again:\n" + rep);
-                break;
+        if (active) {
+            process(recievedMessage);
+        } else {
+            if (recievedMessage.author.id == "324302699460034561") {
+                process(recievedMessage);
+            } else {
+                return
+            }
         }
+
     }
 })
