9b3f6b182518937541b2b53b8e720dbb2c9047ff
HKing2802 <croppa1@yahoo.com>
2019-12-23 18:52:25 -0500

    removed framework and rolled back, then attempted mute command.

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
-const { DiscordBot } = require("dynobot-framework");
 
 var active = true;
 
@@ -12,42 +11,64 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 
-var bot = new DiscordBot(auth.token);
+var bot = new Discord.Client();
+bot.login(auth.token);
 
-bot.onEvent('ready', () => {
-    logger.info(`Logged in!`);
+bot.on('ready', () => {
+    logger.info(`Logged in as ${bot.user.tag}!`);
 });
 
+function getPerm(recievedMessage) {
+    var roles = recievedMessage.member.roles;
+    var ret = false;
+    roles.forEach(role => {
+        if (role.name == "admin" || role.name == "dadmin" || role.name == "moderator") {
+            ret = true;
+        }
+    })
+    return ret;
+}
+
+function getMuted(role) {
+    return role.id == "562452717445054474"
+}
+
 function process(recievedMessage) {
-    logger.info(recievedMessage.getContent().substring(1));
-    var cmdfull = recievedMessage.getContent().substring(1);
+    logger.info(recievedMessage.content.substring(1));
+    var cmdfull = recievedMessage.content.substring(1);
     var cmdparsed = cmdfull.split(" ");
     var cmd = cmdparsed[0];
     var args = cmdparsed.splice(1);
 
-    var chan = recievedMessage.getChannel();
-    var author = recievedMessage.getAuthor();
-
+    var chan = recievedMessage.channel;
     switch (cmd) {
+        case 'Ping':
         case 'ping':
-            if (author.getName() == "Icenoft") {
+            if (recievedMessage.author.username == "Icenoft") {
                 chan.send("Imagine .ping");
             } else {
                 chan.send("Pong!");
             }
             break;
-        /*case 'ep':
-        case 'endProcess':
+        case 'ep':
         case 'endprocess':
-            if (author.getId() == "324302699460034561") {
-                chan.send("Killing Myself...");
-                error("Ending Process");
+        case 'endProcess':
+            if (recievedMessage.author.id == "324302699460034561") {
+                chan.send("Done");
+                bot.destroy()
             } else {
                 chan.send("You must be Admin to do that!");
             }
-            break;*/
+            break;
+        case 'repeat':
+            var rep = ""
+            for (var i = 0; i < args.length; i++) {
+                rep = rep + args[i] + " ";
+            }
+            chan.send("Here it is again:\n" + rep);
+            break;
         case 'shutdown':
-            if (author.getId() == "324302699460034561") {
+            if (recievedMessage.author.id == "324302699460034561") {
                 chan.send("Stopping responses");
                 active = false;
             } else {
@@ -55,37 +76,41 @@ function process(recievedMessage) {
             }
             break;
         case 'activate':
-            if (author.getId() == "324302699460034561") {
+            if (recievedMessage.author.id == "324302699460034561") {
                 chan.send("Restarting responses");
                 active = true;
             } else {
                 chan.send("You must be Admin to do that!");
             }
             break;
-        case 'getPerm':
-            var roles = recievedMessage.getAuthorRoles();
-            roles.forEach(role => {
-                if (role.getName() == "admin" || role.getName() == "dadmin" || role.getName() == "moderator") {
-                    chan.send("Approved");
-                    return;
+        case 'mute':
+            var perm = getPerm(recievedMessage);
+            if (perm) {
+                if (recievedMessage.mention_everyone) {
+                    chan.send("You can't mute everyone!");
+                    break;
+                } else if (!recievedMessage.mentions) {
+                    chan.send("You need to mention someone");
+                } else {
+                    var memb = recievedMessage.mentions.members
+                    chan.send(memb[0].user.username);
+                    memb.addRole(recievedMessage.guild.roles.find(r => r.id = "562452717445054474"));
                 }
-            })
-            chan.send("Done");
-            break;
+                break;
+            }
     }
-            
 }
 
-bot.onEvent('message', (recievedMessage) => {
-    if (recievedMessage.getAuthor() == bot.getClient()) {
+bot.on('message', (recievedMessage) => {
+    if (recievedMessage.author == bot.user) {
         return
         //prevent responding to its own message
     }
-    if (recievedMessage.getContent().substring(0, 1) == ".") {
+    if (recievedMessage.content.substring(0, 1) == ".") {
         if (active) {
             process(recievedMessage);
         } else {
-            if (recievedMessage.getAuthor().getId() == "324302699460034561") {
+            if (recievedMessage.author.id == "324302699460034561") {
                 process(recievedMessage);
             } else {
                 return
