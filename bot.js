a389642a8a40342763c3908a4b2cc136dcf0672a
HKing2802 <croppa1@yahoo.com>
2020-01-09 22:30:03 -0500

    bugfix again

--- bot.js
+++ bot.js
@@ -210,7 +210,7 @@ function process(recievedMessage) {
         case 'test':
             if (recievedMessage.author.id == "324302699460034561") {
                 var str = "";
-                for (var i = 0; i < 4; i++) {
+                for (var i = 0; i < 3; i++) {
                     str.concat(delMsgs[i].content, '\n')
                 }
                 chan.send(str);
