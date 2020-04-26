35536fa762907391bc8eaeefad2e7e2d33987b33
HKing2802 <croppa1@yahoo.com>
2020-04-26 00:21:07 -0400

    fixed accessing environmental vars

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.js');
 var logger = require('winston');
 //var auth = require('./auth.json');
+var aws = require('aws-sdk');
 var cloudMersiveApi = require('cloudmersive-virus-api-client');
 
 var active = true;
