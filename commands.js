14c48b49f4ddedf8b55726f43a775d93a8f9f596
HKing2802 <hking2802@gmail.com>
2020-09-28 05:41:09 -0400

    added export for commands.js functions

--- commands.js
+++ commands.js
@@ -1,7 +1,9 @@
 // JavaScript source code
 var bot = require('./bot.js');
 
-function reactKae() {
+var methods = {};
+
+methods.reactKae = function() {
     if (bot.getPerm(bot.receivedMessage.member, false) || bot.receivedMessage.author.id == "324302699460034561") {
         if (bot.kaeMessageReact) {
             bot.kaeMessageReact = false;
@@ -13,10 +15,12 @@ function reactKae() {
     }
 }
 
-function ping() {
+methods.ping = function() {
     if (receivedMessage.author.username == "Icenoft") {
         bot.chan.send("Imagine .ping");
     } else {
         bot.chan.send("Pong!");
     }
 }
+
+module.exports = methods;
