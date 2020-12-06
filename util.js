716a7f8d0903f6ed94c86ef1f99eb39775123c73
HKing2802 <hking2802@gmail.com>
2020-12-06 17:08:51 -0500

    added filter attachment functions

--- util.js
+++ util.js
@@ -32,5 +32,27 @@ function help(chan) {
     chan.send(embed);
 }
 
+function isBlacklisted(filename) {
+    for (let i = 0; i < file_blacklist.length; i++) {
+        if (filename.endsWith(file_blacklist[i]))
+            return true
+    }
+
+    return false
+}
+
+function filterAttachment(message) {
+    let abuseAttachement = message.attachments.find(attachment => isBlacklisted(attachment.name))
+    if (abuseAttachement !== undefined) {
+        message.delete()
+            .then(msg => msg.channel.send(`Sorry ${msg.author}, I deleted file because it's file-type `
+                + "is blacklisted in our spam filter"))
+        return true
+    }
+
+    return false
+}
+
 exports.getPerm = getPerm;
 exports.help = help;
+exports.filterAttachment = filterAttachment;
