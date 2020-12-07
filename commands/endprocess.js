9ff90fc0ae7b832440613ce10ebfa8a532389603
HKing2802 <hking2802@gmail.com>
2020-12-07 12:32:53 -0500

    added base functions to set template for getFunctions

---
+++ commands/endprocess.js
@@ -0,0 +1,12 @@
+
+const name = "endprocess";
+
+function main(cmd, args) {
+    // TODO: verify
+    // TODO: destroy bot
+    // TODO: flush any changes to db or files
+    // TODO: close db connection
+}
+
+exports.name = name;
+exports.main = main;
