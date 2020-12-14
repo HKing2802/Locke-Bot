9470fd1b5c3a2e0726a2859a84a21394be1d4817
HKing2802 <hking2802@gmail.com>
2020-12-14 17:37:09 -0500

    added function contracts

--- util.js
+++ util.js
@@ -1,8 +1,14 @@
 // Set of utility functions for Lockebot
-const config = require('./config.json')
-const package = require('./package.json')
+const config = require('./config.json');
+const Discord = require('discord.js');
 
-
+/**
+ * Checks the permissions of the member
+ * Function specific to Locke
+ * @param {Discord.GuildMember} member The member to check
+ * @param {boolean} boolHelp Boolean if helpers will pass the check
+ * @returns {boolean}
+ */
 function getPerm(member, boolHelp) {
     if (member.roles.cache.has(config.modRoleID) || member.roles.cache.has(config.dadminRoleID) || member.roles.cache.has(config.adminRoleID)) {
         return true;
@@ -13,15 +19,24 @@ function getPerm(member, boolHelp) {
     }
 }
 
+/**
+ * Checks if a file ends with a certain extension
+ * @param {string} filename - filename to check
+ * @returns {boolean}
+ */
 function isBlacklisted(filename) {
     for (let i = 0; i < file_blacklist.length; i++) {
         if (filename.endsWith(file_blacklist[i]))
             return true
     }
-
     return false
 }
 
+/**
+ * Checks and deletes a message if the attached file is blacklisted
+ * @param {Discord.Message} message The message to check
+ * @returns {boolean}
+ */
 function filterAttachment(message) {
     let abuseAttachement = message.attachments.find(attachment => isBlacklisted(attachment.name))
     if (abuseAttachement !== undefined) {
