f83d154bad4e7360047d80b236b3db4e35b11a07
HKing2802 <hking2802@gmail.com>
2020-12-14 02:15:10 -0500

    added test for ping command

--- test/commandsTest.js
+++ test/commandsTest.js
@@ -1,5 +1,31 @@
-var assert = require('assert');
+const assert = require('assert');
+const testUtil = require('../discordTestUtility/discordTestUtility.js');
+const Discord = require('Discord.js');
+const processor = require('../commands/processor.js');
 
 describe('ping', function () {
-    
+    it('returns message', function (done) {
+        const client = new Discord.Client();
+        const guild = testUtil.createGuild(client);
+        const channel = new testUtil.testChannel(guild);
+        const commands = processor.getFunctions(["ping"]);
+        channel.send(".ping")
+            .then((m) => {
+                processor.process(m, commands)
+                    .then(() => {
+                        const message = channel.getMessage(channel.lastMessageID);
+                        assert.equal(message.content, "Pong!");
+                        client.destroy();
+                        done()
+                    })
+                    .catch((err) => {
+                        client.destroy();
+                        done(err);
+                    });
+            })
+            .catch((err) => {
+                client.destroy();
+                done(err);
+            });
+    });
 });
