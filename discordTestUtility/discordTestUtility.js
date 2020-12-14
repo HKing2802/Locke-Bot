95d80c2e21e365f082d04cb4292fbb15bbfeca50
HKing2802 <hking2802@gmail.com>
2020-12-14 01:41:52 -0500

    added contracts and export for TestChannel in class overrides

--- discordTestUtility/discordTestUtility.js
+++ discordTestUtility/discordTestUtility.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
 const classOverrides = require('./testClassOverrides.js');
 
+/**
+ * Captures the provided stream
+ * @param {stream} stream The stream to capture
+ */
 function captureStream(stream) {
     var oldWrite = stream.write;
     var buf = '';
@@ -19,6 +23,14 @@ function captureStream(stream) {
     };
 }
 
+/**
+ * Creates a Discord User object
+ * @param {Discord.Client} client The instantiating client
+ * @param {string} username The username of the user
+ * @param {string} discriminator The 4 digit discriminator for the user
+ * @param {boolean} bot Flag if the user is a bot
+ * @param {Object} extraData extra data to be provided to the object
+ */
 function createUser(client, username, discriminator, bot = false, extraData = {}) {
     let data;
     if (extraData.id) {
@@ -31,12 +43,28 @@ function createUser(client, username, discriminator, bot = false, extraData = {}
 }
 
 // roles - array of role ids
+/**
+ * Creates a Discord GuildMember object
+ * @param {Discord.Client} client The instantiating client
+ * @param {Discord.Guild} guild The guild the member belongs to
+ * @param {Discord.User} user the user the member is based off of
+ * @param {Array} roles an array of role ids to be added to the member
+ * @param {string} nickname the nickname of the member
+ * @param {Object} extraData extra data to be provided to the object
+ * @returns {Discord.GuildMember}
+ */
 function createMember(client, guild, user, roles = [], nickname = undefined, extraData = {}) {
     const member = new classOverrides.TestMember(client, {user: user, roles: roles, nick: nickname, id: user.id, ...extraData}, guild);
     guild.members.add(member);
     return member;
 }
 
+/**
+ * Creates a Discord Role object and adds it to the provided guild
+ * @param {Discord.Client} client the instantiating client
+ * @param {Discord.Guild} guild the guild the role belongs to
+ * @param {Object} extraData extra data to be provided to the object
+ */
 function createRole(client, guild, extraData = {}) {
     if (extraData.id && extraData.position) {
         const id = extraData.id;
@@ -61,6 +89,11 @@ function createRole(client, guild, extraData = {}) {
     }
 }
 
+/**
+ * Creates a Discord Guild with an @everyone role
+ * @param {Discord.Client} client - the instantiating client
+ * @param {import('discord.js').Snowflake} id - the id for the server
+ */
 function createGuild(client, id) {
     if (typeof id === 'undefined') {
         id = Discord.SnowflakeUtil.generate();
@@ -76,7 +109,7 @@ exports.createUser = createUser;
 exports.createMember = createMember;
 exports.createRole = createRole;
 exports.createGuild = createGuild;
-
+exports.testChannel = classOverrides.TestChannel;
 
 /* all require message
  * 
