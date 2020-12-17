451919fa82f03d183a6e0c8a28c526bb0a6583ef
HKing2802 <hking2802@gmail.com>
2020-12-17 16:46:03 -0500

    finished log function and changed getLogChannels to checkLogChannels

--- util.js
+++ util.js
@@ -1,17 +1,23 @@
 // Set of utility functions for Lockebot
 const config = require('./config.json');
 const Discord = require('discord.js');
-const logger = require('winston');
+const winston = require('winston');
 const file_blacklist = require('./file_blacklist.json');
 
-let LOG_CHANNELS;
-
-// Initializes the logger transport
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
+// Initializes the logger and transports
+const defaultLogger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            filename: 'error.log',
+            level: 'error'
+        })
+    ]
+})
 
 /**
  * Checks the permissions of the member
@@ -67,17 +73,17 @@ async function filterAttachment(message) {
 /**
  * Constructs a map of ids for logging channels. Returns Map<GuildID, ChannelID> or string on error
  * @param {Array<Array<string>>} idList Nested array that contains [GuildID, ChannelID]
- * @returns {Map<string, string>|string}
+ * @returns {Array<Array<string>>|string}
  */
-function getLogChannels(idList) {
+function checkLogChannels(idList) {
     if (!(Array.isArray(idList))) {
-        return `Logging channel not set up properly, array is of type ${typeof idList}`;
+        return `Logging channel not set up properly, parameter is of type ${typeof idList}`;
     } else if (idList.length <= 0) {
-        return "Logging channel idList is empty";
+        return "Logging channel Array is empty";
     } else if (!(Array.isArray(idList[0]))) {
         return `Logging channel not set up properly, array is of type ${typeof idList[0]}`;
     } else {
-        return new Map(idList);
+        return idList;
     }
 }
 
@@ -87,57 +93,53 @@ function getLogChannels(idList) {
  * @param {Discord.Client} [client] The instantiating client, can be omitted if not logging to channels
  * @param {boolean} [channel] Boolean if the message is also logged to logging channels
  * @param {string} [level] The level of the log, can be info, warn or error. Defaults to info
- * @param {Map<string, string>} logChannelOverride A map of <GuildID, ChannelID> to override the default logging channels defined in the cofig.
- * @returns {void}
+ * @param {Array<Array<string>>} [logChannelOverride] A nested array that contains [[GuildID, ChannelID], ...] to override the default logging channels defined in the cofig
+ * @param {Winston.logger} [logger] The logger and transports for the function to log to
+ * @returns {Promise<Discord.Message>|Promise<undefined>}
  */
-function log(content, client, channel = true, level = 'info', logChannelOverride) {
+async function log(content, client, channel = true, level = 'info', logChannelOverride, logger = defaultLogger) {
     // Checks if logging channel ids are loaded and loads if not present or if there is an override provided
     let logChannels;
-    logChannelOverride ? logChannels = logChannelOverride : logChannels = LOG_CHANNELS;
+    if (logChannelOverride) logChannels = logChannelOverride;
     if (!logChannels) {
-        logChannels = getLogChannels(config.logChannels);
-        if (!(logChannels instanceof Map)) {
+        logChannels = checkLogChannels(config.logChannel);
+        if (!(Array.isArray(logChannels))) {
             channel = false;
-            logger.warn(`Could not load logging channel ids: ${logChannels}, skipping channel log...`);
+            logger.warn(`Could not load logging channel ids: ${logChannels}. Skipping channel log...`);
+        }
+    } else if (logChannelOverride && logChannels) {
+        logChannels = checkLogChannels(logChannels);
+        if (!(Array.isArray(logChannels))) {
+            channel = false;
+            logger.warn(`Could not load logging channel override ids: ${logChannels}. Skipping channel log...`);
         }
     }
 
-    // overwrites the global config if log channels were updated and override is not given
-    if (!logChannelOverride && logChannels != LOG_CHANNELS) LOG_CHANNELS = logChannels;
-
     // logs to console
-    switch (level) {
-        case 'warn':
-            logger.warn(content);
-            break;
-        case 'error':
-            logger.error(content);
-            break;
-        case 'info':
-        default:
-            logger.info(content);
-            break;
-    }
+    logger.log(level, content);
 
     // logs to channel
     if (channel && client) {
-        logChannels.forEach((value, key, map) => {
-            const guild = client.guilds.fetch(key);
+        return await Promise.all(logChannels.map(async (arr) => {
+            const guild = client.guilds.cache.get(arr[0]);
             if (!guild) {
-                logger.warn(`Could not load guild for ID ${key} and channel id ${value}`)
+                logger.warn(`Could not load guild for ID ${arr[0]} and channel ID ${arr[1]}`);
             } else {
-                const channel = guild.channels.fetch(value);
+                const channel = guild.channels.cache.get(arr[1]);
                 if (!channel) {
-                    logger.warn(`Could not load channel for ID ${key} in guild ${guild.name}`);
+                    logger.warn(`Could not load channel for ID ${arr[1]} in guild ${guild.name}`);
                 } else {
-                    channel.send(content);
+                    return channel.send(content)
+                        .then((m) => { return m });
                 }
             }
-        })
+        }));
     }
 }
 
 exports.getPerm = getPerm;
 exports.filterAttachment = filterAttachment;
-exports.getLogChannels = getLogChannels;
 exports.log = log;
+exports.testing = {
+    checkLogChannels: checkLogChannels,
+};
