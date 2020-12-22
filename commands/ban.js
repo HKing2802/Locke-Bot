45504e7f326138f9eca54f292a7ff9e0396cad3b
HKing2802 <hking2802@gmail.com>
2020-12-22 00:19:43 -0500

    various updates from testing

--- commands/ban.js
+++ commands/ban.js
@@ -13,19 +13,23 @@ const type = "Moderation";
 
 /**
  * Gets the reason for ban from the arguments
- * Strips the mention of the member being banned
+ * Strips the mention or ID of the member being banned
  * @param {Array<string>} args The arguments provided to the command and split by processor
  * @param {GuildMember} target The target of the ban
  * @returns {string}
  */
 function getReason(args, target) {
     const name = `<@!${target.id}>`;
+    const id = `${target.id}`;
     let reason = "";
 
     for (let i = 0; i < args.length; i++) {
-        if (args[i].indexOf(name) == -1)
-            reason += `${args[i]} `;
-        else
+        if (args[i].indexOf(name) == -1) {
+            if (args[i].indexOf(id) == -1)
+                reason += `${args[i]} `;
+            else
+                reason += `${args[i].substr(id.length)} `;
+        } else
             reason += `${args[i].substr(name.length)} `;
     }
     return reason.trim();
@@ -35,31 +39,48 @@ function getReason(args, target) {
  * Checks to ensure that the target can be banned, and performs the ban
  * @param {Message} message The message received by the bot
  * @param {Array<string>} args The arguments provided to the command and split by processor
- * @param {GuildMember} target The target of the ban
+ * @param {GuildMember} target The target member to ban
  * @returns {boolean}
  */
-function ban(message, args, target) {
-    if (getPerm(target, true)) {
-        chan.send("Can't ban a staff member");
-        return false
+async function ban(message, args, target) {
+    if (util.getPerm(target, true)) {
+        return message.channel.send("Can't ban a staff member")
+            .then(() => { return false });
     }
 
     let reason = getReason(args, target);
 
     return target.ban({ reason: `${reason} - Banned by ${message.author.tag}` })
         .then((m) => {
-            if (reason == "")
-                chan.send(`Banned ${m.user.tag | m.tag | m}, No reason given.`);
-            else
-                chan.send(`Banned ${m.user.tag | m.tag | m} for ${reason}`)
+            let msg;
+            let tag;
+            if (m.user == undefined) {
+                if (m.tag == undefined) {
+                    tag = m;
+                } else {
+                    tag = m.tag;
+                }
+            } else {
+                tag = m.user.tag;
+            }
 
-            util.log(`${message.author.tag} banned ${m.user.tag | m.tag | m} for ${reason}`, message.client, true);
-            return true;
+            if (reason == "")
+                msg = `Banned ${tag}, No reason given.`;
+            else
+                msg = `Banned ${tag} for ${reason}`;
+
+            return message.channel.send(msg)
+                .then(() => {
+                    util.log(`${message.author.tag} banned ${tag} for ${reason}`, message.client, true);
+                    return true;
+                });
         })
         .catch(err => {
-            chan.send(`Unable to ban the member`);
-            util.log(`Unable to ban member ${target.user.tag}\n By: ${message.author.tag}\n Reason: ${reason}\n Error: ${err}`, undefined, false, 'error');
-            return false;
+            return message.channel.send(`Unable to ban the member`)
+                .then(() => {
+                    util.log(`Unable to ban member ${target.user.tag} By: ${message.author.tag} Reason: ${reason} Error: ${err}`, undefined, false, 'error');
+                    return false;
+                });
         });
 };
 
@@ -67,26 +88,24 @@ function ban(message, args, target) {
  * Checks message author permission and if a mention/ID is given
  * @param {Message} message The message received by the bot
  * @param {Array<string>} args The arguments provided to the command and split by processor
- * @returns {boolean}
+ * @returns {boolean|undefined}
  */
-function main(message, args) {
-    const chan = message.channel;
+async function main(message, args) {
     if (util.getPerm(message.member)) {
-        if (message.mention_everyone) {
-            chan.send("I can't Ban everyone");
-            return false
+        if (message.mentions.everyone) {
+            return message.channel.send("I can't Ban everyone")
+                .then(() => { return false });
         } else {
-            let target = message.guild.members.resolve(message.mentions.members.first())
-            if (target) return ban(message, args, target);
-            target = message.guild.members.resolve(args[0]);
-            if (target) return ban(message, args, target);
+            const target = message.guild.members.resolve(message.mentions.members.first())
+            if (target) return await ban(message, args, target);
+            const IDtarget = message.guild.members.resolve(args[0]);
+            if (IDtarget) return await ban(message, args, IDtarget);
             else {
-                chan.send("No member or ID specified");
-                return false;
+                return message.channel.send("No member or ID specified")
+                    .then(() => { return false });
             }
         }
     }
-    return false
 }
 
 exports.main = main;
