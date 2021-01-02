5c977aea0af90feb6319132bbbfa66d4819dbca6
HKing2802 <hking2802@gmail.com>
2021-01-01 19:23:00 -0500

    rewrote to work with xdevapi version of mysql

--- src/db.js
+++ src/db.js
@@ -1,50 +1,80 @@
-
-const mysql = require('mysql');
+const mysqlx = require('@mysql/xdevapi');
 const auth = require('../auth.json');
-const { log } = require('./util.js');
+const { log } = require('../src/util.js');
+const SqlString = require('sqlstring');
 
-const CONNECTED = false;
+let CONNECTED = false;
+let session;
 
-// Database auth
-const con = mysql.createConnection({
+const db_config = {
     host: auth.db_host,
     user: auth.db_user,
     password: auth.db_pass,
-    database: 'lockebot_db'
-});
+    schema: 'lockebot_db',
+    port: 33060
+};
 
 /**
  * Connects to the database
- * @returns {mysql.database}
+ * @param {Object} sessionConfig The configuration object to connect to the database
+ * @param {string} sessionConfig.host The host address of the server
+ * @param {string} sessionConfig.user The username of the client
+ * @param {string} sessionConfig.password The password of the client
+ * @param {string} sessionConfig.schema The database schema for the client to use
+ * @param {number} sessionConfig.port The port of the connection
  */
-function db_connect() {
-    con.connect(function (err) {
-        if (err) {
-            log(`Could not connect to Database: ${err}`, undefined, false, "error");
-            return;
-        } else {
-            log("Connected to Database");
+async function db_connect(sessionConfig = db_config) {
+    if (CONNECTED && session !== undefined) {
+        log('Attempting to connect to database while already connected. Disconnecting...', undefined, false, 'warn');
+        db_disconnect();
+    }
+
+    return mysqlx.getSession(db_config)
+        .then(s => {
             CONNECTED = true;
-            return con;
-        }
-    })
+            log('Connected to database');
+            session = s;
+            return;
+        })
+        .catch(err => {
+            log(`Could not connect to Database: ${err}`, undefined, false, 'error');
+            return;
+        });
+}
+
+/**
+ * Gets if the database is connected
+ * @returns {boolean}
+ */
+function getConnected() {
+    return CONNECTED;
 }
 
 /**
  * Disconnects from the database
  */
-function db_disconnect() {
-    con.end(function (err) {
-        if (err) {
-            log(`Could not disconnect from Database: ${err}`, undefined, false, "error");
-        } else {
-            log("Disconnected from Database");
-            CONNECTED = false;
-        }
-    });
+async function db_disconnect() {
+    try {
+        await session.close();
+        log('Disconnected from Database');
+        CONNECTED = false;
+    } catch (error) {
+        log('Could not disconnect from Database: ${error}', undefined, false, 'error');
+    }
+}
+
+/**
+ * Sanitizes and builds a SQL query with the connected database
+ * Throws error if not connected to database, check if connected by checking db.connected;
+ * @param {string} query The SQL query
+ * @returns {mysqlx.SqlExecute}
+ */
+function buildQuery(query) {
+    if (!CONNECTED) throw Error('Not connected to a Database');
+    return session.sql(SqlString.format(query));
 }
 
 exports.connect = db_connect;
 exports.disconnect = db_disconnect;
-exports.connected = CONNECTED;
-exports.con = con;
+exports.buildQuery = buildQuery;
+exports.connected = getConnected;
