55346d326d4a66cc304e3271371d977c3638d27a
HKing2802 <hking2802@gmail.com>
2021-01-05 11:55:16 -0500

    added function contracts and fixes from testing

--- modules/events/messageProcess.js
+++ modules/events/messageProcess.js
@@ -2,25 +2,48 @@
  */
 const config = require('../../config.json');
 const util = require('../../src/util.js');
-const { process } = require('../../commands/processor.js');
+const { process, getFunctions } = require('../../commands/processor.js');
+const { Message, Client } = require('discord.js');
 
+let commands;
+
+/**
+ * Filters and processes message;
+ * @param {Message} message The message received by the client
+ * @returns {Promise<boolean|undefined>}
+ */
 async function messageProcess(message) {
-    if (util.filterAttachment(message))
-        return;
+    if (await util.filterAttachment(message)) return;
 
     if (message.content.substring(0, 1) == config.prefix) {
         // checks if bot response is active and is in a guild and not a DM
-        if (config.active == 'true' && message.guild != null) {  
-            process(message);
+        if (config.active && message.guild != null) {
+            if (!commands) commands = getFunctions(config.commands);
+            return process(message, commands)
+                .then((status) => { return status })
+                .catch(err => {
+                    util.log(`Error on processing message: ${err}`, undefined, false, 'error');
+                    return false;
+                });
 
         // checks if message author is author and then processes even if active is false
         } else if (message.author.id == config.authorID) {  
-            process(message);
+            if (!commands) commands = getFunctions(config.commands);
+            return process(message, commands)
+                .then((status) => { return status })
+                .catch(err => {
+                    util.log(`Error on processing message: ${err}`, undefined, false, 'error');
+                    return false;
+                });
         } else
             return;
     }
 }
 
+/**
+ * Starts the message event handler
+ * @param {Client} client The client to start listening for messages
+ */
 function main(client) {
     // sets up event listener
     client.on('message', (message) => {
@@ -29,4 +52,6 @@ function main(client) {
 }
 
 exports.main = main;
-exports.messageProcess = messageProcess;
+exports.testing = {
+    messageProcess: messageProcess
+}
