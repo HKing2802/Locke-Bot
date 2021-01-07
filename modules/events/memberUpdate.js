557ceb5dd5fdea6c61c4e0345c58c4a757d77412
HKing2802 <hking2802@gmail.com>
2021-01-07 17:40:34 -0500

    Split the checkNick function into two functions for use in memberAdd

--- modules/events/memberUpdate.js
+++ modules/events/memberUpdate.js
@@ -4,16 +4,20 @@ const config = require('../../config.json');
 const { log } = require('../../src/util.js');
 const re = /[^\x00-\x7F]/g;
 
-async function checkNick(oldMember, newMember, client) {
-    if (oldMember.nickname === newMember.nickname || newMember.nickname === null) return false;
-    if (config.nicknamePass.includes(newMember.id)) return false;
-
-    const newNick = newMember.nickname.replace(re, '');
-    if (newNick != newMember.nickname) {
-        newMember.setNickname(newNick);
+function checkNick(member, client) {
+    const newNick = member.nickname.replace(re, '');
+    if (newNick != member.nickname) {
+        member.setNickname(newNick);
         log(`Force changed a member's nickname to ${newNick}`, client);
         return true;
     }
+}
+
+async function compareNick(oldMember, newMember, client) {
+    if (oldMember.nickname === newMember.nickname || newMember.nickname === null) return false;
+    if (config.nicknamePass.includes(newMember.id)) return false;
+
+    return checkNick(newMember, client);
 }
 
 function main(client) {
@@ -24,6 +28,7 @@ function main(client) {
 }
 
 exports.main = main;
+exports.checkNick = checkNick;
 exports.testing = {
-    checkNick: checkNick
+    compareNick, compareNick
 }
