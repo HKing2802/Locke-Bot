4bfc4779c6682ef29aa1237ad7c3836cc16fabca
HKing2802 <hking2802@gmail.com>
2021-01-20 16:15:29 -0500

    updates from testing

--- modules/timed/auto-unmute.js
+++ modules/timed/auto-unmute.js
@@ -6,8 +6,11 @@ const db = require('../../src/db.js');
 const events = require('events');
 const moment = require('moment');
 const { Client } = require('discord.js');
+const { log } = require('../../src/util.js');
 
-class unmuteEvents extends events {}
+const unmuteEvents = new events();
+
+let UNMUTED = false;
 
 /**
  * Performs the unmute on the member
@@ -17,14 +20,14 @@ class unmuteEvents extends events {}
  * @returns {boolean}
  */
 async function unmute(client, userID, member) {
-    // callback function for checking and unmuting a member
-    // should be vaguely similar to unmute command
+    await db.buildQuery(`DELETE FROM muted_users WHERE user_id = ${userID}`).execute();
 
     const target = client.guilds.cache.get(config.guildID).members.cache.get(userID);
     if (!target) return false;
     else {
         if (target.roles.cache.has(config.humanRoleID)) return false;
         else {
+            UNMUTED = true;
             await target.roles.remove(config.mutedRoleID);
             await target.roles.add(config.humanRoleID);
             if (member) await target.roles.add(config.memberRoleID);
@@ -37,97 +40,88 @@ async function unmute(client, userID, member) {
  * Sets up the Timeout for the next unmute
  * @param {Client} client The client of the bot
  * @param {number} userID The ID of the user to unmute
- * @returns {Array<number, NodeJS.Timeout>}
+ * @returns {Object<number, NodeJS.Timeout>}
  */
 async function setupUnmute(client, userID) {
-    // sets up timer for unmute
-    // queries db and calculates time
-    // may be integrated into controller and use getNextUnmute
-    // returns timeout object
-
-    return db.buildQuery(`SELECT member, time_unmute FROM muted_users where user_id = ${userID}`)
+    let timeoutTime;
+    let toObject;
+    await db.buildQuery(`SELECT member, time_unmute FROM muted_users where user_id = ${userID}`)
         .execute(async result => {
-            const unmuteTime = moment(result[1]);
-            const timeoutTime = unmuteTime.diff(moment());
+            const unmuteTime = moment(result[1]).add(5, 'h');
+            timeoutTime = unmuteTime.diff(moment());
 
             if (timeoutTime > 0) {
-                const toObject = setTimeout(() => {
+                UNMUTED = false;
+                toObject = setTimeout(() => {
                     unmute(client, userID, Boolean(result[0]));
-                }, timeoutTime)
-                return [timeoutTime, toObject];
+                }, timeoutTime);
             } else {
                 await unmute(client, userID, Boolean(result[0]));
-                const nextUser = getNextUnmute();
-                return setupUnmute(client, nextUser);
             }
         })
-        .catch(err => { throw err });
+        .catch(err => { log(`Error in setting up unmute timer: ${err}`, client, false, 'error') });
+    return { time: timeoutTime, obj: toObject };
+}
+
+/**
+ * Updates the unmute timer
+ * @param {Client} client The client of the bot
+ * @param {NodeJS.Timeout} nextTimeout The timeout object for the next unmute
+ * @returns {Object<number, NodeJS.Timeout>}
+ */
+async function updateUnmute(client, nextTimeout) {
+    log('updating next unmute', client, false);
+    clearTimeout(nextTimeout);
+    let data;
+    do {
+        nextUser = await getNextUnmute();
+        if (!nextUser) return;
+        data = await setupUnmute(client, nextUser);
+    } while (data.obj === undefined);
+    return data;
 }
 
 /**
  * Controls the unmute module and handles changes to the next unmute time
  * @param {Client} client The client of the bot
  * @param {NodeJS.Timeout} startTimeout The timeout object of the next unmute
- * @param {number} startUser The id of the next user to unmute
- * @param {Moment} startTime The moment object of the time the next unmute happens
- * @returns {undefined}
  */
-async function controller(client, startTimeout, startUser, startTime) {
-    // stores next timer object
-    // sets up listener for timed mute event
-    // sets up listener for garbage collection event
-    // on either event recheck next unmute time
+function controller(client, startTimeout) {
+    let nextTimeout = startTimeout;
 
-    let nextTimeout = startTimeout;     // timeout object
-    let nextUser = startUser;           // number userID
-    let nextTime = startTime;           // moment object
-
-    unmuteEvents.on('mute', (muteTime) => {
-        // muteTime should be a moment object
-
-        if (muteTime.diff(nextTime) < 0) {
-            clearTimeout(nextTimeout);
-            nextUser = getNextUnmute();
-            const data = setupUnmute(client, nextUser);
-            nextTime = data[0];
-            nextTimeout = data[1];
-        }
+    unmuteEvents.on('mute', () => {
+        updateUnmute(client, nextTimeout);
     });
     unmuteEvents.on('garbageCollection', () => {
-        const newUser = getNextUnmute();
-        if (newUser !== nextUser) {
-            clearTimeout(nextTimeout);
-            nextUser = newUser;
-            const data = setupUnmute(client, nextUser);
-            nextTime = data[0];
-            nextTimeout = data[1];
-        }
+        updateUnmute(client, nextTimeout);
     });
 }
 
 /**
  * Gets the next userID that needs to be unmuted from the database
- * @returns {number}
+ * @returns {Promise<number>|undefined}
  */
-function getNextUnmute() {
-    // function to query database for next unmute
-    // returns user ID of next unmute
-
+async function getNextUnmute() {
     let user;
     let time;
-    return db.buildQuery(`SELECT user_id, time_unmute FROM muted_users`)
+    await db.buildQuery(`SELECT user_id, time_unmute FROM muted_users`)
         .execute(result => {
             if (result[1] != null) {
                 if (result[1] < time) {
                     user = result[0];
                     time = result[1];
+                } else if (time === undefined) {
+                    user = result[0];
+                    time = result[1];
                 }
             }
         })
-        .then(() => {
-            return user;
-        })
         .catch(err => { throw err });
+    return user;
+}
+
+function getStatus() {
+    return UNMUTED;
 }
 
 /**
@@ -144,9 +138,12 @@ async function initialize(client) {
 }
 
 exports.main = initialize;
+exports.events = unmuteEvents;
 exports.testing = {
     unmute: unmute,
     getNextUnmute: getNextUnmute,
     controller: controller,
-    setupUnmute: setupUnmute
+    setupUnmute: setupUnmute,
+    getStatus: getStatus,
+    updateUnmute: updateUnmute
 }
