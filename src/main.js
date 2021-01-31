05744492d7bfdf2ed1abf2b2b4c33ed7f947a03d
HKing2802 <hking2802@gmail.com>
2021-01-31 02:15:37 -0500

    reworked main to be started by start.js instead of directly

--- src/main.js
+++ src/main.js
@@ -3,28 +3,43 @@ const auth = require('../auth.json');
 require('hjson/lib/require-config');
 const config = require('../config.hjson');
 const db = require('./db.js');
-const { log } = require('./util.js');
+const { log, sleep } = require('./util.js');
 const moduleHandler = require('./module_handler.js');
 const { sweep } = require('../modules/timed/dbGarbageCollection.js');
 
-// bot login
-const client = new Client();
-client.login(auth.token);
+let CLIENT;
 
-client.once('ready', async () => {
-    log(`------------------------------------------------------------------`, client, false);
-    log(`Logged in as ${client.user.tag}`, client, false);
+function bot_init() {
+    // bot login
+    const client = new Client();
+    client.login(auth.token);
 
-    // Connect to Database
-    await db.connect()
-        .then(() => { log(`Connected to Database`, client, false) })
-        .catch(err => { log(`Error in connecting to Database: ${err}`, client, false, 'error') });
+    // Stores client for use in shutdown
+    CLIENT = client;
 
-    // startup garbage collection
-    await sweep(client);
+    client.once('ready', async () => {
+        log(`------------------------------------------------------------------`, client, false);
+        log(`Logged in as ${client.user.tag}`, client, false);
+        
+        // Connect to Database
+        await db.connect()
+            .then(() => { log(`Connected to Database`, client, false) })
+            .catch(err => { log(`Error in connecting to Database: ${err}`, client, false, 'error') });
 
-    // initializes modules
-    const modules = moduleHandler.getModules(config.modules);
-    moduleHandler.startModules(modules, client);
-    log(`Modules Initialized. Startup complete`, client, false);
-});
+        // startup garbage collection
+        await sweep(client);
+
+        // initializes modules
+        const modules = moduleHandler.getModules(config.modules);
+        const started = await moduleHandler.startModules(modules, client);
+        log(`${started}/${modules.size} Modules Initialized. Startup complete`, client, false);
+    });
+}
+
+async function shutdown() {
+    //const endprocess = require('../commands/endprocess.js');
+    endprocess.events.emit('end');
+}
+
+exports.init = bot_init;
+exports.shutdown = shutdown;
