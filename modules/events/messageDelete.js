85320a09b436681586ce5d899b00341a5612d94a
HKing2802 <hking2802@gmail.com>
2021-07-18 22:13:17 -0400

    Updated database interaction to use new format and prepared statements Updated error logging to log to discord log channels

--- modules/events/messageDelete.js
+++ modules/events/messageDelete.js
@@ -4,25 +4,44 @@ const db = require('../../src/db.js');
 const { log } = require('../../src/util.js');
 const moment = require('moment');
 
+// sql prepared statements
+const insertMessageStatement = db
+    .getSessionSchema()
+    .getTable('messages')
+    .insert(['id', 'user_id', 'channel_id', 'send_time', 'content']);
+
+const insertEditsStatement = db
+    .getSessionSchema()
+    .getTable('edits')
+    .insert(['id', 'msg_id', 'num', 'edit_time', 'content']);
+
 async function recordDeleted(message) {
     if (message.author.bot) return false;
     else {
         // inserts message to table
         if (!(db.connected())) {
-            log('Could not log deleted message: not connected to database', undefined, false, 'warn');
+            log('Could not log deleted message: Not connected to database', undefined, false, 'warn');
             return false;
         }
         const sendTime = moment(message.createdTimestamp).format('YYYY-MM-DD HH:mm:ss');
-        db.buildQuery(`INSERT INTO messages(id, user_id, channel_id, send_time, content) VALUES (${message.id}, ${message.author.id}, ${message.channel.id},'${sendTime}', '${message.content}')`).execute()
-            .catch(err => { throw err });
-         
-        // checks for edits
+        insertMessageStatement
+            .values(message.id, message.author.id, message.channel.id, sendTime, message.content)
+            .execute()
+            .catch(err => {
+                log(`Error in deleted message database insertion: ${err}`, message.client, false, 'error');
+            });
+
+        // checks for and records edits
         if (message.edits.length > 1) {
             for (let i = 1; i < message.edits.length; i++) {
                 const editTime = moment(message.edits[i].createdAt).format('YYYY-MM-DD HH:mm:ss');
                 const editId = String(message.id) + String(i);
-                db.buildQuery(`INSERT INTO edits(id, msg_id, num, edit_time, content) VALUES (${editId}, ${message.id}, ${i}, '${editTime}', '${message.edits[i].content}')`).execute()
-                    .catch(err => { log(`Error in database query: ${err}`, undefined, false, 'error') });
+                insertEditsStatement
+                    .values(editID, message.id, i, editTime, message.edits[i].content)
+                    .execute()
+                    .catch(err => {
+                        log(`Error in deleted message edits insertion: ${err}`, message.client, false, 'error');
+                    });
             }
         }
 
