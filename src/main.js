5695ff4c659965153ac4db926c7b5d1aa0901e76
HKing2802 <hking2802@gmail.com>
2021-07-18 23:48:45 -0400

    fixed initialization error with loading the garbage collection module and database calls before a connection is made

--- src/main.js
+++ src/main.js
@@ -3,36 +3,35 @@ const auth = require('../auth.json');
 require('hjson/lib/require-config');
 const config = require('../config.hjson');
 const db = require('./db.js');
-const { log, sleep } = require('./util.js');
+const { log } = require('./util.js');
 const moduleHandler = require('./module_handler.js');
-const { sweep } = require('../modules/timed/dbGarbageCollection.js');
 
-let CLIENT;
 
 function bot_init() {
     // bot login
     const client = new Client();
     client.login(auth.token);
 
-    // Stores client for use in shutdown
-    CLIENT = client;
-
     client.once('ready', async () => {
         log(`------------------------------------------------------------------`, client, false);
         log(`Logged in as ${client.user.tag}`, client, false);
         
         // Connect to Database
-        await db.connect()
-            .then(() => { log(`Connected to Database`, client, false) })
+        db.connect()
+            .then(async () => {
+                log(`Connected to Database`, client, false);
+
+                const { sweep } = require('../modules/timed/dbGarbageCollection.js');
+
+                // startup garbage collection
+                await sweep(client);
+
+                // initializes modules
+                const modules = moduleHandler.getModules(config.modules);
+                const started = await moduleHandler.startModules(modules, client);
+                log(`${started}/${modules.size} Modules Initialized. Startup complete`, client, false);
+            })
             .catch(err => { log(`Error in connecting to Database: ${err}`, client, false, 'error') });
-
-        // startup garbage collection
-        await sweep(client);
-
-        // initializes modules
-        const modules = moduleHandler.getModules(config.modules);
-        const started = await moduleHandler.startModules(modules, client);
-        log(`${started}/${modules.size} Modules Initialized. Startup complete`, client, false);
     });
 }
 
